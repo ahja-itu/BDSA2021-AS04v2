824468e68e2552ed2b0bf2146224918b279b16eb
Andreas Wachs Hjalager <ahja@itu.dk>
2021-10-10 21:37:02 +0200

    Implement and test tagrepo delete method

--- Assignment4.Entities.Tests/TagRepositoryTests.cs
+++ Assignment4.Entities.Tests/TagRepositoryTests.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Assignment4.Core;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Assignment4.Entities.Tests
@@ -6,5 +11,123 @@ namespace Assignment4.Entities.Tests
     public class TagRepositoryTests
     {
 
+        private readonly SqliteConnection _sqliteConnection;
+        private readonly KanbanContext _context;
+        private TagRepository _repo;
+
+        public TagRepositoryTests()
+        {
+            _sqliteConnection = new SqliteConnection("Filename=:memory:");
+            _sqliteConnection.Open();
+            var builder = new DbContextOptionsBuilder<KanbanContext>();
+            builder.UseSqlite(_sqliteConnection);
+            _context = new KanbanContext(builder.Options);
+            _context.Database.EnsureCreated();
+            _repo = new TagRepository(_context);
+
+
+
+            // base tag, no associated tasks
+            var tag = new Tag { Id = 1, Name = "Buzzword" };
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+
+
+            // prepare test user
+            var user = new User { Id = 1, Name = "Bob", Email = "uncle@bob.com"};
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            // tag that is in use
+            tag = new Tag { Id = 2, Name = "Urgent" };
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+            
+            // create a task for the test user
+
+            var tags = new Collection<Tag>();
+            tags.Add(tag);
+            var task = new Task { Id = 1, Title = "Important task", Description = "This is important", AssignedTo = user, tags = tags};
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public void Create_unique_tag_name_should_be_created()
+        {
+            var entity = new TagCreateDTO { Name = "unique tag name" };
+
+            (Response response, int tid) = _repo.Create(entity);
+
+            Assert.Equal(Response.Created, response);
+            Assert.Equal(tid, 3);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void Create_tag_with_empty_name_should_return_bad_request(string name)
+        {
+            var entity = new TagCreateDTO { Name = name };
+
+            (Response response, int tid) = _repo.Create(entity);
+
+            Assert.Equal(Response.BadRequest, response);
+            Assert.Equal(tid, -1);
+        }
+
+        [Fact]
+        public void Create_attempt_creating_tag_with_existing_name_should_return_conflict()
+        {
+            var entity = new TagCreateDTO { Name = "Buzzword" };
+
+            (Response response, int tid) = _repo.Create(entity);
+
+            Assert.Equal(Response.Conflict, response);
+            Assert.Equal(tid, -1);
+        }
+
+        [Fact]
+        public void Delete_unused_tag_without_force_should_return_deleted()
+        {
+            var tid = 1;
+
+            var response = _repo.Delete(tid);
+            
+            Assert.Equal(Response.Deleted, response);
+        }
+
+        [Fact]
+        public void Delete_tag_does_not_exist_should_return_response_not_found()
+        {
+            var tid = 1337;
+
+            var response = _repo.Delete(tid);
+
+            Assert.Equal(Response.NotFound, response);
+        }
+
+        [Fact]
+        public void Delete_tag_in_use_without_force_should_return_conflict()
+        {
+            var tid = 2;
+
+            var response = _repo.Delete(tid, false);
+
+            Assert.Equal(Response.Conflict, response);
+        }
+
+        [Fact]
+        public void Delete_tag_in_use_with_force_should_return_deleted()
+        {
+            var tid = 2;
+
+            var response = _repo.Delete(tid, true);
+
+            Assert.Equal(Response.Deleted, response);
+        }
+
+        
     }
 }
--- Assignment4.Entities/TagRepository.cs
+++ Assignment4.Entities/TagRepository.cs
@@ -1,6 +1,113 @@
+using System.Collections.Generic;
+using Assignment4.Core;
+using System.Linq;
+
 namespace Assignment4.Entities
 {
-    public class TagRepository
+    public class TagRepository : ITagRepository
     {
+
+        private readonly KanbanContext _context;
+
+        public TagRepository(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        /*
+
+            [X] Tags which are assigned to a task may only be deleted using the force.
+            [X] Trying to delete a tag in use without the force should return Conflict.
+            [X] Trying to create a tag which exists already should return Conflict.
+
+
+            [X] Trying to update or delete a non-existing entity should return NotFound.
+            Create, Read, and Update should return a proper Response.
+            Your are not allowed to write throw new ... - use the Response instead.
+            If a task, tag, or user is not found, return null.
+
+        */
+
+        public (Response Response, int TagId) Create(TagCreateDTO tag)
+        {
+            if (IsEmpty(tag.Name))
+            {
+                return (Response.BadRequest, -1);
+            }
+
+            var existingTag = _context.Tags.FirstOrDefault(t => t.Name == tag.Name);
+
+            if (existingTag != null)
+            {
+                return (Response.Conflict, -1);
+            }
+
+            var createEntity = new Tag { Name = tag.Name };
+
+            _context.Tags.Add(createEntity);
+            _context.SaveChanges();
+
+            return (Response.Created, createEntity.Id);
+        }
+
+        public Response Delete(int tagId, bool force = false)
+        {
+            var tag = _context.Tags.Find(tagId);
+            if (tag == null)
+            {
+                return Response.NotFound;
+            }
+
+            var numTagUsages = _context.Tasks
+                                .Where(t => t.tags.Contains(tag))
+                                .Count();
+
+            var tasksWithTag = from task in _context.Tasks
+                               where task.tags.Contains(tag)
+                               select task;
+
+            if (numTagUsages > 0)
+            {
+                if (force)
+                {
+
+                    foreach(var task in tasksWithTag)
+                    {
+                        task.tags.Remove(tag);
+                        _context.Tasks.Update(task);
+                        _context.SaveChanges();
+                    }
+
+                    _context.SaveChanges();
+
+                }
+                else
+                {
+                    return Response.Conflict;
+                }
+            }
+
+            _context.Tags.Remove(tag);
+            _context.SaveChanges();
+            return Response.Deleted;
+        }
+
+        public TagDTO Read(int tagId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IReadOnlyCollection<TagDTO> ReadAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Response Update(TagUpdateDTO tag)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private bool IsEmpty(string str) => str == null || str.Trim().Length == 0;
     }
 }
